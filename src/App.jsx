db58833e80efa7c1190d2b802f6174f4b5c0069d
Dashrath001 <dashrathwankhede74@gmail.com>
2024-01-12 01:01:00 +0530

    portfolio steup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import styles from './App.module.css'
+// import "@fontsource/roboto"
+// import "@fontsource/outfit"
+
+function App() {
+ 
+
+  return (
+    <div className={styles.App}>Dashrath Wankhede</div>
+    
+  )
+}
+
+export default App
